2acaf99326f980645606b137d5fbd60d02712243
kherasakshi <sakshikhera456@gmail.com>
2025-01-06 23:43:02 +0530

    Update 01_Variables.js

--- Basics_1/01_Variables.js
+++ Basics_1/01_Variables.js
@@ -1,1 +1,22 @@
 
+/ variable and constants
+// variable are like an empty container or we can say like an memory space and constants are like an
+// value that you can't change the value assigned to that variable
+
+const firstId = 45678;
+let classRollNo = 29;
+var cityId = 15002;
+
+// firstId = 10000;
+console.log(firstId);
+// that gives you an error we can't change the value assigning using const keyword
+
+// using let can re-declare the value but can't reassign the value ;
+ classRollNo = 30;
+console.log(classRollNo);
+
+// using var we can re-declare and re - assign the value both but now we can't use var keyword as it might bought bug in our code 
+var cityId = 18000;
+console.log(cityId);
+
+console.table([firstId, classRollNo, cityId]);
