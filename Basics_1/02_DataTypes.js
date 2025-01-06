5f2343463d1f1a2024e7e914a0f27c87fb8bf69c
kherasakshi <sakshikhera456@gmail.com>
2025-01-07 00:03:03 +0530

    Update 02_DataTypes.js

--- Basics_1/02_DataTypes.js
+++ Basics_1/02_DataTypes.js
@@ -1,1 +1,11 @@
+Data Types are of two Types Primitive data types and Non-Promitive Data-types 
+Number 
+String 
+Boolean
+null
+undefined
+BigInt
+and Non -Primitive data types is 
+Object one of the Main No-Primitive Data Type
 
+DataTypes conversion 
